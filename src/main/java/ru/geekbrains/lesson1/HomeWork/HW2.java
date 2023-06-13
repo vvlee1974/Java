37e9068bfd5a6045a7b2db9057870fc816bee90a
vvlee <vvlee@gmail.com>
2023-06-13 19:55:10 +0500

    change HW2.java

--- src/main/java/ru/geekbrains/lesson1/HomeWork/HW2.java
+++ src/main/java/ru/geekbrains/lesson1/HomeWork/HW2.java
@@ -3,58 +3,31 @@ package ru.geekbrains.lesson1.HomeWork;
 import java.util.Scanner;
 
 public class HW2 {
+
     public static void main(String[] args) {
-        //int[] array = {1, 2, 1, 2, -1, 1, 3, 1, 3, -1, 0};
-        //getArray(String str);
-        System.out.println("Введите через пробел числа, последняя цифра должна быть 0: ");
+        System.out.println("Введите последовательсность чисел, заканчивающуюся нулём: ");
         Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
 
-        String str = scanner.nextLine();
-
+        System.out.println("getSum(a) = " + getSum(a));
         scanner.close();
-
-        getArray(str);
-
-        if (getArray(str)[getArray(str).length - 1] != 0) {
-            System.out.println("Ошибка ввода. Строка должна заканчиваться цифрой 0");
-        } else if (getArray(str)[getArray(str).length - 1] == 0) {
-            System.out.println("getSum(array) = " + getSum(getArray(str)));
-        }
     }
 
     /**
-     * @param array
+     * @param a
      * @return sum
-     * @apiNote Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных чисел,
-     * после которых следует отрицательное число.
+     * @apiNote
      */
-
-    private static int getSum(int[] array) {
+    private static int getSum(int a) {
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < array.length; i++) {
-            a = array[i];
-            if (a > 0) b = a;
-            if (a < 0) sum += b;
+
+        while (a != 0) {
+            int b = scanner.nextInt();
+            if (a > 0 && b < 0) sum += a;
+            a = b;
         }
+        scanner.close();
         return sum;
-    }
-
-    private static int[] getArray(String str) {
-        String s = str.trim().replaceAll("\\s+", " ");
-
-        String[] arr = s.split(" ");
-
-        int[] array = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            try {
-                array[i] = Integer.parseInt(arr[i]);
-            } catch (NumberFormatException nfe) {
-                System.out.println("Ошибка ввода. Введены не числа.");
-                break;
-            }
-        }
-        return array;
     }
 }
