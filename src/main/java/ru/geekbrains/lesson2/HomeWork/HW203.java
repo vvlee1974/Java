bf22973790f3edbeebf6030a7cfbc79089249cbd
vvlee <vvlee@gmail.com>
2023-06-14 00:56:56 +0500

    add HW203.java

---
+++ src/main/java/ru/geekbrains/lesson2/HomeWork/HW203.java
@@ -0,0 +1,52 @@
+package ru.geekbrains.lesson2.HomeWork;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class HW203 {
+    public static void main(String[] args) {
+        int size = 6;
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(31) - 15;
+        }
+        System.out.println("Начальный массив: " + Arrays.toString(array));
+        //System.out.println(Arrays.toString(array));
+        System.out.println("Сумма индесков двухзначных элементов массива = " + getIndexSum(array));
+        System.out.println("Итоговый массив = " + Arrays.toString(getFinalArray(array)));
+    }
+
+    /**
+     * @param array входящий сгенерированный массив
+     * @return Итоговый массив
+     * @apiNote Заменить отрицательные элементы на сумму индексов двузначных элементов массива.
+     */
+
+    private static int[] getFinalArray(int[] array) {
+        int[] arr = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                arr[i] = getIndexSum(array);
+            } else arr[i] = array[i];
+        }
+
+        return arr;
+    }
+
+    /**
+     * @param array входящий сгенерированный массив
+     * @return Сумма индексов двухзначных элементов массива
+     * @apiNote Получить сумму индексов двухзначных элементов массива
+     */
+    private static int getIndexSum(int[] array) {
+        int indexSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] / 10 != 0) indexSum += i;
+        }
+        return indexSum;
+    }
+}
