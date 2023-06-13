f17f4cabedf09192652aa5e460bf96b04fa49ce8
vvlee <vvlee@gmail.com>
2023-06-13 22:56:19 +0500

    add eX201.JAVA

--- src/main/java/ru/geekbrains/lesson2/ClassWork/Ex201.java
+++ src/main/java/ru/geekbrains/lesson2/ClassWork/Ex201.java
@@ -1,5 +1,21 @@
 package ru.geekbrains.lesson2.ClassWork;
 
 public class Ex201 {
+    public static void main(String[] args) {
+        int[] array = {2, 6, 5, 8, 5, 6, 2};
+        System.out.println(palindrom(array));
+    }
 
+    /**
+     * @apiNote Дан массив целых чисел. Верно ли, что массив является симметричным.
+     * @param array входной массив
+     * @return результат
+     */
+    private static boolean palindrom(int[] array) {
+        int len = array.length - 1;
+        for (int i = 0; i < array.length/2; i++) {
+            if(array[i] != array[len - i]) return false;
+        }
+        return true;
+    }
 }
