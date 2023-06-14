09b2ce4263d45668a01feea576183239972f04f3
vvlee <vvlee@gmail.com>
2023-06-14 22:28:29 +0500

    change ex304.java

--- src/main/java/ru/geekbrains/lesson3/ClassWork/ex304/Cube.java
+++ src/main/java/ru/geekbrains/lesson3/ClassWork/ex304/Cube.java
@@ -11,8 +11,4 @@ public class Cube {
     private Integer size;
     private String color;
     private String material;
-
-    public Integer volumeCube() {
-        return size * size * size;
-    }
 }
--- src/main/java/ru/geekbrains/lesson3/ClassWork/ex304/Main.java
+++ src/main/java/ru/geekbrains/lesson3/ClassWork/ex304/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Cube cube1 = new Cube(2, "желтый", "деревянный");
@@ -16,18 +17,6 @@ public class Main {
         cubeList.add(cube5);
         cubeList.add(cube6);
         System.out.println("cubeList = " + cubeList);
-        int k = 0;
-        int volume = 0;
-        int kWood = 0;
-        for (Cube cube : cubeList) {
-            if (cube.getColor().equals("желтый")) {
-                k++;
-                volume += cube.volumeCube();
-            }
-            if (cube.getMaterial().equals("деревянный") && cube.getSize() == 3) {
-                kWood++;
-            }
-        }
         System.out.println("unicumListCube(cubeList) = " + unicumListCube(cubeList));
     }
 
@@ -35,7 +24,7 @@ public class Main {
      * @param cubeList входной список кубов
      * @return уникальная колекция
      */
-    private static Collection unicumListCube(List<Cube> cubeList) {
-        return new HashSet(cubeList);
+    private static Collection<Cube> unicumListCube(List<Cube> cubeList) {
+        return new HashSet<>(cubeList);
     }
 }
