5c82c9877c3a993106b8dbe1a208dde856f58a5a
vvlee <vvlee@gmail.com>
2023-06-19 23:45:35 +0500

    change ex401.ClassWork

--- src/main/java/ru/geekbrains/lesson4/ClassWork/ex401/Ex401.java
+++ src/main/java/ru/geekbrains/lesson4/ClassWork/ex401/Ex401.java
@@ -1,6 +1,5 @@
 package ru.geekbrains.lesson4.ClassWork.ex401;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -15,17 +14,17 @@ public class Ex401 {
         System.out.println("linkedList = " + linkedList);
         scanner.close();
         System.out.println("linkedList.peek() = " + linkedList.peek());
-        System.out.println("linkedList = " + linkedList);
+        System.out.println("linkedList .peek()= " + linkedList);
         System.out.println("linkedList.pop() = " + linkedList.pop());
-        System.out.println("linkedList = " + linkedList);
+        System.out.println("linkedList .pop() = " + linkedList);
         linkedList.push(1);
+        System.out.println("linkedList .push(1) = " + linkedList);
+        linkedList.add(2); // добавляет в конец
+        linkedList.addLast(3); // добавляет в конец
+        linkedList.addFirst(3); // добавляет в начало
         System.out.println("linkedList = " + linkedList);
-        linkedList.add(2);
-        linkedList.addLast(3);
-        linkedList.addFirst(3);
-        System.out.println("linkedList = " + linkedList);
-        linkedList.removeFirst();
-        linkedList.removeLast();
+        System.out.println("linkedList.removeFirst() = " + linkedList.removeFirst());
+        System.out.println("linkedList.removeLast() = " + linkedList.removeLast());
         System.out.println("linkedList = " + linkedList);
         System.out.println("linkedList.contains(3) = " + linkedList.contains(3));
     }
